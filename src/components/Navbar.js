67b93592c73b352b02158b26f612de6f7c27c6f6
Aku Nikula <aku.nikula@gmail.com>
2023-03-15 16:17:33 +0200

    Vanhaa koodia poistettu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,33 +50,3 @@ export default function Navbar({ url }) {
         </nav>
     )
 }
-
-
-
-
-
-
-/* <div className="container-fluid">
-            <a className="navbar-brand" to="/">Verkkokauppa</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                    <li className="nav-item">
-                        <a className="nav-link" to="/">Etusivu</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Tuotteet</a>
-                        <ul className="dropdown-menu" aria-labelledby="dropdown01">
-                            {categories.map(category => (
-                                <li key={category.id}>
-                                    <a className="dropdown-item" to={"/products/" + category.id}>{category.name}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div> */
