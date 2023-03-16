16a459e78b1ff875c1d330eb7d520baa1f4fa5f4
Aku Nikula <aku.nikula@gmail.com>
2023-03-16 13:46:32 +0200

    Näkymä lähemmäksi Figma-designia

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,27 @@ import React from 'react'
 
 export default function Home() {
 
-
+    
   return (
     <div className='homecontainer container-fluid'>
+        <div className='product-thumbnail'>
+            
+        </div>
+        <div className='product-thumbnail'>
+
+        </div>
+        <div className='product-thumbnail'>
+
+        </div>
+        <div className='product-thumbnail'>
+
+        </div>
+        <div className='product-thumbnail'>
+
+        </div>
+        <div className='product-thumbnail'>
+
+        </div>
         
     </div>
   )
