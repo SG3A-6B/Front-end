6607fdb4554fe733a70b67acc6420ca755a4538b
eemilto <117357193+eemilto@users.noreply.github.com>
2023-03-22 10:24:35 +0200

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import className from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import {div} from 'react-bootstrap'
 
 
 
@@ -8,29 +9,50 @@ export default function Home() {
     
   return (
     <div className='homecontainer container-fluid'>
+      <div class="row">
+      <div class="col-3">
         <div className='product-thumbnail'>
             
         </div>
+        </div>
+        <div class="col-3">
         <div className='product-thumbnail'>
 
         </div>
+        </div>
+        <div class="col-3">
         <div className='product-thumbnail'>
 
         </div>
+        </div>
+        <div class="col-3">
         <div className='product-thumbnail'>
 
         </div>
+        </div>
+        <div class="row">
+        <div class="col-3">
         <div className='product-thumbnail'>
             
             </div>
+            </div>
+            <div class="col-3">
             <div className='product-thumbnail'>
     
             </div>
+            </div>
+            <div class="col-3">
             <div className='product-thumbnail'>
     
             </div>
+            </div>
+            <div class="col-3">
             <div className='product-thumbnail'>
+              </div>
+              </div>
+              </div>
               </div>
 </div>
   )
 }
+
