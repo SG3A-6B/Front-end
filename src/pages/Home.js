bd27c3523e3016e1f788dc201ad0abeb04a5ab39
eemilto <117357193+eemilto@users.noreply.github.com>
2023-03-23 11:09:32 +0200

    Näkymä lähemmäksi Figma-designia

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,59 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import {homecontainer, row, col} from 'react-bootstrap'
 
 
 
 export default function Home() {
 
-
+    
   return (
-    <div className='homecontainer container-fluid'>
-        
+    <div className="homecontainer container-fluid">
+      <div class="row">
+        <div class="col-3">
+          <div className='product-thumbnail'>
+            
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+
+          </div>
+        </div>
+      <div class="row">
+        <div class="col-3">
+          <div className='product-thumbnail'>
+            
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+    
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+    
+          </div>
+        </div>
+        <div class="col-3">
+          <div className='product-thumbnail'>
+
+          </div>
+        </div>
+      </div>
     </div>
+</div>
   )
 }
+
