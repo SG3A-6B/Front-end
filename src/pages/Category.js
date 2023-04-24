aa9869432edbaf2e50041212f54c5d87f9e12f4d
eemilto <117357193+eemilto@users.noreply.github.com>
2023-04-24 11:31:38 +0300

    muutoksia

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -4,7 +4,7 @@ import Card from '../components/Card';
 export default function Category({url}) {
   return (
     
-    <div className='container-fluid content-container'>
+    <div className='container-fluid content-container containeeer'>
     <div className='category-thumbnail'>
       <Card url={url} />
     
