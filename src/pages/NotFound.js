67babca7783a142be5a6d09499213f8b2c7c0aa6
Aku Nikula <aku.nikula@gmail.com>
2023-05-02 12:44:56 +0300

    Notfound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div id="notfound">NOTHING HERE :-DDD</div>
+    <>
+      <div id="notfound">404 - Page not found</div>
+      <img src={require('../images/7.png')} className='imgholder' />
+    </>
   )
 }
